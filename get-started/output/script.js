e6b0bf7651647c287a0ad1e4ed41d0a0918a3f0c
Wixden <watchonlytutorial@gmail.com>
2023-10-04 02:47:26 +0600

    Typescript setup and first hello world complete

---
+++ get-started/output/script.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("I'm getting started");
+const country = 'Bangladesh';
+console.log(country);
+const countries = ["Bangladesh", "India", "Pakistan"];
+console.log(countries);
+const person = { name: "Akhtar", age: 25, role: "Admin" };
